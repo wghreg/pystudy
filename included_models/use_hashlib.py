a70c6f50d9599bbca460640a9e20cb05dd70b6e1
wghreg <wghregister@gmail.com>
2018-03-30 11:48:30 +0800

    t

--- included_models/use_hashlib.py
+++ included_models/use_hashlib.py
@@ -75,3 +75,21 @@ def calc_md5(password):
 
 设计一个验证用户登录的函数，根据用户输入的口令是否正确，返回True或False：
 '''
+# -*- coding: utf-8 -*-
+db = {
+    'michael': 'e10adc3949ba59abbe56e057f20f883e',
+    'bob': '878ef96e86145580c38c87f0410ad153',
+    'alice': '99b1c2188db85afee403b1536010c2c9'
+}
+
+def login(user, password):
+    pass
+
+# 测试:
+assert login('michael', '123456')
+assert login('bob', 'abc999')
+assert login('alice', 'alice2008')
+assert not login('michael', '1234567')
+assert not login('bob', '123456')
+assert not login('alice', 'Alice2008')
+print('ok')
